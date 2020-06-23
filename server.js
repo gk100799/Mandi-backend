5144f256d342ecb0e93a125a1a52d5d15907235c
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-06-24 01:26:58 +0530

    added user model

--- server.js
+++ server.js
@@ -1,71 +1,16 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { buildSchema } from 'graphql';
 import mongoose from 'mongoose'
 
-import Post from './models/posts'
+import graphqlSchema from './graphql/schema/index'
+import graphqlResolvers from './graphql/resolvers/index'
+
 
 var app = express();
 
-var schema = buildSchema(`
-  type Post {
-    _id: ID!
-    username: String
-    typeofpost: String
-    description: String
-  }
-
-  type Query {
-    posts: [Post!]!
-  }
-  
-  input PostInput {
-    username: String!
-    typeofpost: String!
-    description: String!
-  }
-
-
-  type Mutation {
-    createPost(postInput: PostInput): Post
-  }
-`);
-
-var root = {
-  hello: () => 'Hello world!',
-  posts: () => {
-    return Post.find()
-      .then(posts => {
-        return posts.map(post => {
-          return { ...post._doc, _id: post.id };
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  },
-  
-  createPost: args => {
-    const post = new Post({
-      username: args.postInput.username,
-      typeofpost: args.postInput.typeofpost,
-      description: args.postInput.description
-    });
-    return post.save()
-     .then(result => {
-      return { ...result._doc,}
-     })
-     .catch(err => {
-      console.log(err);
-      throw err;
-     })
-  }
-};
-
-
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
+  schema: graphqlSchema,
+  rootValue: graphqlResolvers,
   graphiql: true,
 }));
 
