a5f7e8781e98fa8facb5ffc48d0597d6d49ed4a5
gk100799 <55948183+gk100799@users.noreply.github.com>
2020-06-25 23:58:21 +0530

    Update resolverHelper.js

--- graphql/resolvers/resolverHelper.js
+++ graphql/resolvers/resolverHelper.js
@@ -4,9 +4,3 @@ const transformResponse = item => {
     return { ...item._doc, _id: item.id, createdAt: dateToString(item._doc.createdAt), updatedAt: dateToString(item._doc.updatedAt) }
 }
 module.transformResponse = transformResponse
-import { dateToString } from '../../helpers/date'
-
-export const transformResponse = item => {
-    return { ...item._doc, _id: item.id, createdAt: dateToString(item._doc.createdAt), updatedAt: dateToString(item._doc.updatedAt) }
-}
-
